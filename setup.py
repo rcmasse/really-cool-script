4717745cdd0bb14bf1310a0ef0ef8f9453053539
Robert Masse <rcmasse57@gmail.com>
2018-03-13 22:44:32 -0700

    tryna figure out if this is gonna make something pip-installable

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+  name = 'really-cool-script', # This is the name of our PyPI package
+  version = '0.1',              # Update the version number for new releases
+  scripts = ['henloworld']      # The name of our scripts, plus the command for calling it(?)
+)
